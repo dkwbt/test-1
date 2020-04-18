1a1548878a0f3ef3c9e05e79e7cdbc9487b12d88
dkwbt <4709696+dkwbt@users.noreply.github.com>
2020-04-18 15:30:19 +0900

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CustomTextField = () => {
+  return (
+    <div>
+      <input type="text" value="hoge" />
+    </div>
+  );
+};
+
+export default CustomTextField;
